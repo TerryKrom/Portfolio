05e21f4177ebf11da31f192034ba2fdc3a7bf083
TerryKrom <terrykrom1@gmail.com>
2024-12-15 14:04:37 -0300

    Fix: header container responsivity

--- portfolio/src/components/containers/headerContainer/headerContainer.jsx
+++ portfolio/src/components/containers/headerContainer/headerContainer.jsx
@@ -6,15 +6,14 @@ const HeaderContainer = () => {
     return (
         <>
             <div className="container row header-container">
-                <div className="main-text col-lg-6 col-md-4 col-sm-6 col-xs-auto">
+                <div className="main-text col-lg-6 col-md-6 col-sm-12 col-xs-auto">
                     <h1 className='text-sm-center'>Hi, I'm Terry</h1>
                     <h2 className='text-sm-center'>Software Developer</h2>
-                    <p className='text-sm-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quisquam laudantium doloribus dolore quam tempore harum vitae nam a!</p>
+                    <p className='text-sm-center'>I'm a passionate software developer driven by technology and innovation. I craft digital solutions that empower users and transform ideas into reality.</p>
                 </div>
-                <div className="col-lg-6 col-md-4 sm-6 col-xs-auto d-flex justify-content-center align-items-center position-relative">
+                <div className="col-lg-6 col-md-6 col-sm-12 col-xs-auto d-flex justify-content-center align-items-center position-relative ">
                     <div className="image d-flex justify-content-center align-items-center">
-                        <div className="blob"></div>
-                        <img src="/profile.png" alt="profile image" className='img-fluid'/>
+                        <img src="/profile-ai.png" alt="profile image" className='img-fluid'/>
                     </div>
                 </div>
             </div>
